356ba5dc88d501fbca4e4a7b9fadd36e1ef6e1c5
PiotrSobczak <piosobc@gmail.com>
2017-10-10 01:05:29 +0200

    minor changes

--- svm.py
+++ svm.py
@@ -51,11 +51,11 @@ if __name__ == '__main__':
     #svr_lin = SVR(kernel='linear', C=1000)
     #svr_poly = SVR(kernel='poly', C=1000, degree=2, gamma=0.5)
 
-    max_gamma_val = 0.16
-    min_gamma_val = 0.12
+    max_gamma_val = 0.1
+    min_gamma_val = 0.3
     max_c_val = 400
     min_c_val = 150
-    steps = 20
+    steps = 30
     repeats = 2
 
     gamma_vals = np.linspace(max_gamma_val, min_gamma_val, steps)
@@ -66,7 +66,7 @@ if __name__ == '__main__':
 
     name = "Gaussian"
 
-    for i,c_val in enumerate(c_vals):
+    for i,gamma_val in enumerate(gamma_vals):
         for j in range(repeats):
             if j ==0:
                 X_train = datasetX[:TRAIN_SIZE]
@@ -78,7 +78,7 @@ if __name__ == '__main__':
                 Y_train = datasetY[TRAIN_SIZE:]
                 X_val = datasetX[:TRAIN_SIZE]
                 Y_val = datasetY[:TRAIN_SIZE]
-            classifier = SVR(kernel='rbf', C=c_val, gamma=0.1325)
+            classifier = SVR(kernel='rbf', C=325, gamma=gamma_val)
             classifier.fit(X_train, Y_train)
 
             #Making predictions on train set and setting negative results to zero
@@ -96,9 +96,9 @@ if __name__ == '__main__':
     val_err_his = val_err_his / 2
     train_err_his = train_err_his / 2
 
-    plt.plot(c_vals,train_err_his,label='train')
-    plt.plot(c_vals,val_err_his,label='val')
-    plt.xlabel('c', fontsize=16)
+    plt.plot(gamma_vals,train_err_his,label='train')
+    plt.plot(gamma_vals,val_err_his,label='val')
+    plt.xlabel('gamma', fontsize=16)
     plt.ylabel('error', fontsize=16)
     plt.legend()
     plt.show()
